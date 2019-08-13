be6cb5e181c7b59bba58decbce615a6864dd446d
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-13 16:27:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   newarr = []
   while array[counter] do
-    push.newarr(array[counter] * array[counter])
+    newarr.push(array[counter] * array[counter])
     counter += 1
   end
   p newarr
